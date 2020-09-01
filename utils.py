1e6f9055c450e7faa6718c0ae927d0379dac67c2
franciscobarber <45273569+franciscobarber@users.noreply.github.com>
2020-08-31 21:50:29 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -79,7 +79,7 @@ def cross_entropy_loss(logits, expected):
 
 def optimise_and_sample(init_z, module, data, std,m_shape, is_training):
   """Optimising generator latent variables and sample."""
-eps = tf.random_normal(tf.shape(mean), 0, 1, dtype=tf.float32)
+  eps = tf.random_normal(tf.shape(mean), 0, 1, dtype=tf.float32)
   ruido = tf.multiply(std, eps) 
   if module.num_z_iters == 0:
     z_final = init_z
